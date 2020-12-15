05e4450aefafb21c76d52b01e656d8051abf25c8
Maxim <maxgpod@gmail.com>
2020-12-15 15:00:14 -0800

    Simple DISTINCT

---
+++ 8kyu/simple_Distinct.sql
@@ -0,0 +1,13 @@
+--https://www.codewars.com/kata/58111670e10b53be31000108/train/sql
+--
+--For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+--
+--people table schema
+--id
+--name
+--age
+--select table schema
+--age (distinct)
+
+SELECT DISTINCT age
+FROM people;
