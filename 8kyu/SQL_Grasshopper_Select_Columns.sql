b8c6f804ca9a6811d5b5ee8f12d6df9ed2294c8f
Maxim <maxgpod@gmail.com>
2020-12-15 15:48:42 -0800

    SQL Grasshopper: Select Columns

---
+++ 8kyu/SQL_Grasshopper_Select_Columns.sql
@@ -0,0 +1,7 @@
+-- https://www.codewars.com/kata/582365c18917435ab3000020/train/sql
+
+--#Greetings Grasshopper!# Using only SQL, write a query that returns all rows in the custid, custname, and custstate
+--columns from the customers table.
+
+SELECT custid, custname, custstate
+FROM customers;
