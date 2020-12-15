5af01f9fae60593cc090562c23255e650acc06db
Maxim <maxgpod@gmail.com>
2020-12-15 11:10:55 -0800

    Adults only

---
+++ 8kyu_Adults_only.sql
@@ -0,0 +1,14 @@
+
+-- https://www.codewars.com/kata/590a95eede09f87472000213/train/sql
+
+-- In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+-- users table schema
+
+-- name
+-- age
+
+
+SELECT *
+FROM users
+WHERE age >= 18;
